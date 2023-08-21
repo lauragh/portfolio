abe6f4641220d60dc4a0114a369d08d9d169a2f2
Laura García <lgh15@alu.ua.es>
2023-08-21 13:50:17 +0200

    add projects translated to english

--- src/app/pages/projects/game/game.component.ts
+++ src/app/pages/projects/game/game.component.ts
@@ -1,4 +1,8 @@
 import { Component } from '@angular/core';
+import * as english from '../../../../assets/traduccion/en.json';
+import * as spanish from '../../../../assets/traduccion/es.json';
+import { DataService } from 'src/app/services/data.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-game',
@@ -6,5 +10,36 @@ import { Component } from '@angular/core';
   styleUrls: ['./game.component.css']
 })
 export class GameComponent {
+  
+  idioma!: string;
+  translate: any = spanish;
+  
+  ngOnInit(): void {
+    this.getLanguage();
+  }
+
+    constructor(
+    private dataService: DataService,
+  ){
+  }
+
+  getLanguage(){
+    this.dataService.idiomaG.pipe(
+      tap(idioma => {
+      })
+    ).subscribe(idioma => {
+      this.idioma = idioma;
+      this.translateText(idioma);
+    });
+  }
+
+   translateText(idioma: string){
+    if(idioma === 'en'){
+      this.translate = english;
+    }
+    else {
+      this.translate = spanish;
+    }
+  }
 
 }
--- src/app/pages/projects/mobbler/mobbler.component.ts
+++ src/app/pages/projects/mobbler/mobbler.component.ts
@@ -1,10 +1,45 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as english from '../../../../assets/traduccion/en.json';
+import * as spanish from '../../../../assets/traduccion/es.json';
+import { DataService } from 'src/app/services/data.service';
+import { tap } from 'rxjs';
 
 @Component({
   selector: 'app-mobbler',
   templateUrl: './mobbler.component.html',
   styleUrls: ['./mobbler.component.css']
 })
-export class MobblerComponent {
+export class MobblerComponent implements OnInit {
+
+  idioma!: string;
+  translate: any = spanish;
+  
+  ngOnInit(): void {
+    this.getLanguage();
+  }
+
+    constructor(
+    private dataService: DataService,
+  ){
+  }
+
+  getLanguage(){
+    this.dataService.idiomaG.pipe(
+      tap(idioma => {
+      })
+    ).subscribe(idioma => {
+      this.idioma = idioma;
+      this.translateText(idioma);
+    });
+  }
+
+   translateText(idioma: string){
+    if(idioma === 'en'){
+      this.translate = english;
+    }
+    else {
+      this.translate = spanish;
+    }
+  }
 
 }
